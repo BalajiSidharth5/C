1e14e15e6c474944ad5925f5b6fcea62a043ab57
BalajiSidharth5 <balaji.sidharth05@gmail.com>
2025-02-26 15:37:50 -0500

    Add files via upload

---
+++ Projects_C/FileChanging/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define FILENAME "Text.txt"
+
+int main(int argc, char **argv)
+{
+    FILE *fp = NULL;
+    FILE *fp1 = NULL;
+    char c;
+    
+    fp = fopen(FILENAME, "r");
+    if(fp == NULL){
+        return -1;
+    }
+    
+    fp1 = fopen("Temp.txt", "w");
+    if(fp1 == NULL){
+        return -1;
+    }
+    
+    while((c = fgetc(fp)) != EOF){
+        if (islower(c)){
+            c = c - 32;
+        }
+        fputc(c, fp1);
+    }
+    
+    fclose(fp);
+    fclose(fp1);
+    
+    rename("Temp.txt", FILENAME);
+    remove("Temp.txt");
+    
+    fp = fopen(FILENAME, "r");
+    if (fp == NULL){
+        return -1;
+    }
+    
+    while((c=fgetc(fp)) != EOF){
+        printf("%c", c);
+    }
+    
+    fclose(fp);
+    fp = NULL;
+    fp1 = NULL;
+    
+    return 0;
+}
